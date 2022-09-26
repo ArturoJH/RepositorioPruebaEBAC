1bcc04f2b72b78efe12714a371f4cfe971a1d6e6
ArturoJH <113862663+ArturoJH@users.noreply.github.com>
2022-09-25 22:28:59 -0500

    Creacion del primer Script
    Primer scripts con loop

--- ProyectoDeUnity/Assets/Scripts/HolaMundo.cs
+++ ProyectoDeUnity/Assets/Scripts/HolaMundo.cs
@@ -4,18 +4,38 @@ using UnityEngine;
 
 public class HolaMundo : MonoBehaviour
 {
+
+    int x;
     // Start is called before the first frame update
     void Start()
     {
-        print("Algo paso");
-        Debug.Log("Algo paso");
-        Debug.LogWarning("Algo salio medianamente mal");
-        Debug.LogError("Algo salio muy mal");
+        x = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //x = x + 1;
+        //Debug.Log(x);
+
+        Debug.Log("Hola desde Update");
     }
+    private void FixedUpdate()
+    {
+        Debug.LogWarning("Hola desde fixed update cada 50 frames");
+    }
+    private void LateUpdate()
+    {
+        Debug.Log("Hola desde LateUpdate");
+    }
+    private void OnEnable()
+    {
+        Debug.LogWarning("El objeto ha sido habilitado");
+    }
+    private void OnDisable()
+        {
+        Debug.LogWarning("El objeto ha sido inhabilitado");
+        }
+
+
 }
